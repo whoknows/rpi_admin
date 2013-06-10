f6736dcc82a65089e049cfa3851d12c7bb35652c
gcoste <guillaume.coste@livingobjects.fr>
2013-06-10 11:18:32 +0200

    more pages

---
+++ pages/system.php
@@ -0,0 +1,406 @@
+<!-- Main bar -->
+<div class="mainbar">
+
+	<!-- Page heading -->
+	<div class="page-head">
+		<!-- Page heading -->
+		<h2 class="pull-left">System Details
+			<!-- page meta -->
+			<span class="page-meta">Scripts and system management</span>
+		</h2>
+
+
+		<!-- Breadcrumb -->
+		<div class="bread-crumb pull-right">
+			<a href="?page=index"><i class="icon-home"></i> Home</a>
+			<!-- Divider -->
+			<span class="divider">/</span>
+			<a href="#" class="bread-current">System Management</a>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<!-- Page heading ends -->
+
+
+
+	<!-- Matter -->
+
+	<div class="matter">
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<!-- Operating System -->
+				<div class="span4">
+					<div class="widget wred">
+						<!-- Widget title -->
+						<div class="widget-head">
+							<div class="pull-left">Log Files</div>
+							<div class="widget-icons pull-right">
+								<a href="#" class="wminimize"><i class="icon-chevron-up"></i></a>
+								<a href="#" class="wclose"><i class="icon-remove"></i></a>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="widget-content referrer">
+							<!-- Widget content -->
+
+							<table class="table  table-bordered ">
+								<tr>
+									<th><center>#</center></th>
+									<th>File name</th>
+									<th>File Size</th>
+								</tr>
+								<tr>
+									<td><img src="img/icons/metro.png" alt="" />
+									<td>temp.log</td>
+									<td>245Kb</td>
+								</tr>
+								<tr>
+									<td><img src="img/icons/metro.png" alt="" />
+									<td>cpu.log</td>
+									<td>178Kb</td>
+								</tr>
+							</table>
+
+						</div>
+					</div>
+				</div>
+
+
+				<!-- Browsers -->
+				<div class="span4">
+					<div class="widget wlightblue">
+						<!-- Widget title -->
+						<div class="widget-head">
+							<div class="pull-left">Scripts</div>
+							<div class="widget-icons pull-right">
+								<a href="#" class="wminimize"><i class="icon-chevron-up"></i></a>
+								<a href="#" class="wclose"><i class="icon-remove"></i></a>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="widget-content referrer">
+							<!-- Widget content -->
+
+							<table class="table  table-bordered ">
+								<tr>
+									<th><center>#</center></th>
+									<th>Browsers</th>
+									<th>Visits</th>
+								</tr>
+								<tr>
+									<td><img src="img/icons/chrome.png" alt="" />
+									<td>mopidy.sh</td>
+									<td><button class="btn btn-danger">Exec</button></td>
+								</tr>
+								<tr>
+									<td><img src="img/icons/chrome.png" alt="" />
+									<td>archiveLog.sh</td>
+									<td><button class="btn btn-danger">Exec</button></td>
+								</tr>
+							</table>
+
+						</div>
+					</div>
+				</div>
+
+				<!-- Services -->
+				<div class="span4">
+					<div class="widget wviolet">
+						<!-- Widget title -->
+						<div class="widget-head">
+							<div class="pull-left">Services</div>
+							<div class="widget-icons pull-right">
+								<a href="#" class="wminimize"><i class="icon-chevron-up"></i></a>
+								<a href="#" class="wclose"><i class="icon-remove"></i></a>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="widget-content referrer">
+							<!-- Widget content -->
+							<table class="table  table-bordered ">
+								<tr>
+									<th><center>#</center></th>
+									<th>Service name</th>
+									<th>Service status</th>
+									<th>Actions</th>
+								</tr>
+								<tr>
+									<td><img src="img/icons/chrome.png" alt="" />
+									<td>Shairport</td>
+									<td>Runnig</td>
+									<td>
+										<button class="btn btn-success">Start</button>
+										<button class="btn btn-danger">Stop</button>
+										<button class="btn btn-info">Restart</button>
+									</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+
+			<div class="row-fluid">
+
+				<!-- Task widget -->
+				<div class="span5">
+					<div class="widget wblue">
+						<!-- Widget title -->
+						<div class="widget-head">
+							<div class="pull-left">Tasks</div>
+							<div class="widget-icons pull-right">
+								<a href="#" class="wminimize"><i class="icon-chevron-up"></i></a>
+								<a href="#" class="wclose"><i class="icon-remove"></i></a>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="widget-content">
+							<!-- Widget content -->
+							<!-- Task list starts -->
+							<ul class="task">
+
+								<li>
+									<!-- Checkbox -->
+									<input value="check1" type="checkbox">
+									<!-- Task -->
+									Goto Shopping in Walmart <span class="label label-success">Important</span>
+									<!-- Delete button -->
+									<button class="btn btn-mini btn-danger pull-right"><i class="icon-remove"></i></button>
+								</li>
+
+								<li>
+									<!-- Checkbox -->
+									<input value="check1" type="checkbox">
+									<!-- Task -->
+									Download some action movies
+									<!-- Delete button -->
+									<button class="btn btn-mini btn-danger pull-right"><i class="icon-remove"></i></button>
+								</li>
+
+								<li>
+									<!-- Checkbox -->
+									<input value="check1" type="checkbox">
+									<!-- Task -->
+									Read Harry Potter VII Book <span class="label label-important">Very Important</span>
+									<!-- Delete button -->
+									<button class="btn btn-mini btn-danger pull-right"><i class="icon-remove"></i></button>
+								</li>
+
+								<li>
+									<!-- Checkbox -->
+									<input value="check1" type="checkbox">
+									<!-- Task -->
+									Collect cash from friends for camp
+									<!-- Delete button -->
+									<button class="btn btn-mini btn-danger pull-right"><i class="icon-remove"></i></button>
+								</li>
+
+								<li>
+									<!-- Checkbox -->
+									<input value="check1" type="checkbox">
+									<!-- Task -->
+									Sleep till tomorrow everning
+									<!-- Delete button -->
+									<button class="btn btn-mini btn-danger pull-right"><i class="icon-remove"></i></button>
+								</li>
+							</ul>
+							<div class="clearfix"></div>
+
+
+						</div>
+					</div>
+				</div>
+
+
+				<div class="span7">
+					<div class="widget worange">
+						<div class="widget-head">
+							<div class="pull-left">Quick Post</div>
+							<div class="widget-icons pull-right">
+								<a href="#" class="wminimize"><i class="icon-chevron-up"></i></a>
+								<a href="#" class="wclose"><i class="icon-remove"></i></a>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="widget-content">
+							<div class="padd">
+								<div class="form quick-post">
+									<!-- Edit profile form (not working)-->
+									<form class="form-horizontal">
+										<!-- Title -->
+										<div class="control-group">
+											<label class="control-label" for="title">Title</label>
+											<div class="controls">
+												<input type="text" class="input-xlarge" id="title">
+											</div>
+										</div>
+										<!-- Content -->
+										<div class="control-group">
+											<label class="control-label" for="content">Content</label>
+											<div class="controls">
+												<textarea class="input-xlarge" id="content"></textarea>
+											</div>
+										</div>
+										<!-- Cateogry -->
+										<div class="control-group">
+											<label class="control-label">Category</label>
+											<div class="controls">
+												<select>
+													<option value="">- Choose Cateogry -</option>
+													<option value="1">General</option>
+													<option value="2">News</option>
+													<option value="3">Media</option>
+													<option value="4">Funny</option>
+												</select>
+											</div>
+										</div>
+										<!-- Tags -->
+										<div class="control-group">
+											<label class="control-label" for="tags">Tags</label>
+											<div class="controls">
+												<input type="text" class="input-xlarge" id="tags">
+											</div>
+										</div>
+
+										<!-- Buttons -->
+										<div class="form-actions">
+											<!-- Buttons -->
+											<button type="submit" class="btn btn-info">Publish</button>
+											<button type="submit" class="btn">Save Draft</button>
+											<button type="reset" class="btn">Reset</button>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="widget-foot">
+							<!-- Footer goes here -->
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+
+			<div class="row-fluid">
+
+
+				<!-- File Upload widget -->
+				<div class="span4">
+					<div class="widget wviolet">
+						<!-- Widget title -->
+						<div class="widget-head">
+							<div class="pull-left">File Upload</div>
+							<div class="widget-icons pull-right">
+								<a href="#" class="wminimize"><i class="icon-chevron-up"></i></a>
+								<a href="#" class="wclose"><i class="icon-remove"></i></a>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="widget-content">
+							<!-- Widget content -->
+							<!-- File upload list starts -->
+							<ul class="file-upload">
+
+								<li>
+									<!-- Icon with file name -->
+									<strong><i class="icon-upload-alt green"></i> File_Name_Here.Mp3</strong>
+									<!-- Progress bar -->
+									<div class="progress progress-animated progress-striped">
+										<div class="bar bar-success" data-percentage="100"></div>
+									</div>
+									<!-- Upload details -->
+									<div class="file-meta">3.3 of 5MB - 5 mins - 1MB/Sec</div>
+									<!-- Buttons -->
+									<div class="btn-grou1p">
+										<button class="btn btn-mini btn-success"><i class="icon-play"></i> </button>
+										<button class="btn btn-mini btn-primary"><i class="icon-pause"></i> </button>
+										<button class="btn btn-mini btn-danger pull-right"><i class="icon-remove"></i> </button>
+									</div>
+								</li>
+
+								<li>
+									<strong><i class="icon-ok red"></i> Second_File.Mp3</strong>
+									<div class="file-meta">5MB - 5 mins - Completed</div>
+								</li>
+
+								<li>
+									<strong><i class="icon-ok red"></i> Third_File_Here.Mp3</strong>
+									<div class="file-meta">5MB - 5 mins - Stopped</div>
+								</li>
+
+							</ul>
+
+						</div>
+
+						<div class="widget-foot">
+							<button class="btn pull-right">Clear All</button>
+							<div class="clearfix"></div>
+						</div>
+
+					</div>
+				</div>
+
+				<!-- Server Status -->
+				<div class="span4">
+					<div class="widget wlightblue">
+						<!-- Widget title -->
+						<div class="widget-head">
+							<div class="pull-left">Server Status</div>
+							<div class="widget-icons pull-right">
+								<a href="#" class="wminimize"><i class="icon-chevron-up"></i></a>
+								<a href="#" class="wclose"><i class="icon-remove"></i></a>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="widget-content">
+							<!-- Widget content -->
+
+							<table class="table  table-bordered ">
+								<tr>
+									<td>Domain</td>
+									<td>sitedomain.com</td>
+								</tr>
+								<tr>
+									<td>IP Address</td>
+									<td>192.425.2.4</td>
+								</tr>
+								<tr>
+									<td>Disk Space</td>
+									<td>600GB / 60000GB</td>
+								</tr>
+								<tr>
+									<td>Bandwidth</td>
+									<td>1000GB / 2000GB</td>
+								</tr>
+								<tr>
+									<td>PHP Version</td>
+									<td>5.1.1</td>
+								</tr>
+								<tr>
+									<td>MySQL Databases</td>
+									<td>10</td>
+								</tr>
+							</table>
+
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+
+	<!-- Matter ends -->
+
+</div>
